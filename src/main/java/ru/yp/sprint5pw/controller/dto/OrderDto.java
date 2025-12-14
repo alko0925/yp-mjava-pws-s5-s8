c70109b74563ac7860ef4d61ba794e602730f43c
Alexander Korolev <alex.korolev.52rus@gmail.com>
2025-12-15 02:01:48 +0300

    s5: feat: Add RootController: getOrders/getOrder

---
+++ src/main/java/ru/yp/sprint5pw/controller/dto/OrderDto.java
@@ -0,0 +1,44 @@
+package ru.yp.sprint5pw.controller.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDto {
+
+    private Integer id;
+    private List<ItemDto> items = new ArrayList<>();
+    private Long totalSum = 0L;
+
+    public OrderDto(){
+    }
+
+    public OrderDto(Integer id, List<ItemDto> items, Long totalSum) {
+        this.id = id;
+        this.items = items;
+        this.totalSum = totalSum;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List<ItemDto> getItems() {
+        return items;
+    }
+
+    public void setItems(List<ItemDto> items) {
+        this.items = items;
+    }
+
+    public Long getTotalSum() {
+        return totalSum;
+    }
+
+    public void setTotalSum(Long totalSum) {
+        this.totalSum = totalSum;
+    }
+}
