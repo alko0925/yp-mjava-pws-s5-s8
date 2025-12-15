fa66c63a30c2c4b79fb4ba6176104b9eacec7cf9
Alexander Korolev <alex.korolev.52rus@gmail.com>
2025-12-15 03:18:35 +0300

    s5: fix: Fix dependency issue related to Tests

--- src/test/java/ru/yp/sprint5pw/config/PostgreSQLTestcontainer.java
+++ src/test/java/ru/yp/sprint5pw/config/PostgreSQLTestcontainer.java
@@ -1,13 +1,13 @@
 package ru.yp.sprint5pw.config;
 
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.postgresql.PostgreSQLContainer;
 
 public final class PostgreSQLTestcontainer {
 
     @Container
     @ServiceConnection
-    static final PostgreSQLContainer postgreSQLContainer =
-        new PostgreSQLContainer("postgres");
+    static final PostgreSQLContainer<?> postgreSQLContainer =
+        new PostgreSQLContainer<>("postgres");
 }
