d190ce1fdee462083973420dbd45b77ede0bed37
Alexander Korolev <alex.korolev.52rus@gmail.com>
2025-12-16 01:16:46 +0300

    s5: clean: Remove not actual tables from shema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,3 @@
-CREATE TABLE IF NOT EXISTS account
-(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    balance NUMERIC(15, 2) NOT NULL DEFAULT 10000,
-    CONSTRAINT account_balance_non_negative CHECK (balance >= 0)
-);
-
-CREATE TABLE IF NOT EXISTS notification
-(
-    id SERIAL PRIMARY KEY,
-    message VARCHAR(255) NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS products (
     id SERIAL PRIMARY KEY,
     title VARCHAR(400) NOT NULL,
