d3f6f2dd6d15eeea4986926d5a05f35fb3c3679a
Alexander Korolev <alex.korolev.52rus@gmail.com>
2025-12-29 15:13:20 +0300

    s6: refactor: Update ApiExceptionHandler

--- src/main/java/ru/yp/sprint6pw/exception/ApiExceptionHandler.java
+++ src/main/java/ru/yp/sprint6pw/exception/ApiExceptionHandler.java
@@ -1,29 +1,36 @@
 package ru.yp.sprint6pw.exception;
 
-import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.reactive.result.view.Rendering;
+import reactor.core.publisher.Mono;
 
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
     // Обработка 404
-//    @ExceptionHandler(NoSuchElementException.class)
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    public String handleNoSuchElementException(NoSuchElementException e, Model model) {
-//        model.addAttribute("errReason", e.getMessage());
-//        return "error";
-//    }
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Mono<Rendering> handleNoSuchElementException(NoSuchElementException e, Model model) {
+        return Mono.just(
+                Rendering.view("error")
+                        .modelAttribute("errReason", e.getMessage())
+                        .build()
+        );
+    }
 
     // Обработка 400
-//    @ExceptionHandler({IllegalArgumentException.class, ConstraintViolationException.class})
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public String handleIllegalArgumentException(RuntimeException e, Model model) {
-//        model.addAttribute("errReason", e.getMessage());
-//        return "error";
-//    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Mono<Rendering> handleIllegalArgumentException(RuntimeException e, Model model) {
+        return Mono.just(
+                Rendering.view("error")
+                        .modelAttribute("errReason", e.getMessage())
+                        .build()
+        );
+    }
 }
