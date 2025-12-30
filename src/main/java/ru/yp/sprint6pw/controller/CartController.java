576941b88c98ef68655afd2ed2c4ad4bb45df550
Alexander Korolev <alex.korolev.52rus@gmail.com>
2025-12-30 23:08:37 +0300

    s6: refactor: Update CartController: getItems/applyActionToItem

--- src/main/java/ru/yp/sprint6pw/controller/CartController.java
+++ src/main/java/ru/yp/sprint6pw/controller/CartController.java
@@ -2,9 +2,12 @@ package ru.yp.sprint6pw.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.result.view.Rendering;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 import ru.yp.sprint6pw.controller.dto.ItemDto;
-import ru.yp.sprint6pw.model.Cart;
 import ru.yp.sprint6pw.model.Product;
 import ru.yp.sprint6pw.service.CartService;
 import ru.yp.sprint6pw.service.ProductService;
@@ -25,45 +28,51 @@ public class CartController {
         this.cartService = cartService;
     }
 
-//    @GetMapping(value = "/items")
-//    public String getItems(Model model) {
-//
-//        Cart cart = cartService.getCartByUserId(1);
-//        List<ItemDto> items = new ArrayList<>();
-//        Long total = 0L;
-//
-//        if (cart != null) {
-//            for (var cp : cart.getCartProducts()) {
-//                ItemDto item = new ItemDto();
-//                item.setId(cp.getProduct().getId());
-//                item.setTitle(cp.getProduct().getTitle());
-//                item.setDescription(cp.getProduct().getDescription());
-//                item.setImgPath(cp.getProduct().getImgPath());
-//                item.setPrice(cp.getProduct().getPrice());
-//                item.setCount(cp.getQuantity());
-//                items.add(item);
-//            }
-//            total = cart.getTotalCartPrice();
-//        }
-//
-//        model.addAttribute("items", items);
-//        model.addAttribute("total", total);
-//
-//        return "cart";
-//    }
+    @GetMapping(value = "/items")
+    public Mono<Rendering> getItems(Model model) {
 
-//    @PostMapping(value = "/items")
-//    public String applyActionToItem(@RequestParam("id") Integer item_id,
-//                                    @RequestParam("action") String action) {
-//
-//        Product p = productService.getProduct(item_id);
-//
-//        switch (ActionType.valueOf(action)) {
-//            case  MINUS -> cartService.decreaseProductCount(1, p);
-//            case  PLUS -> cartService.increaseProductCount(1, p);
-//            case  DELETE -> cartService.deleteProduct(1, p);
-//        };
-//
-//        return "redirect:/cart/items";
-//    }
+        return cartService.getCartByUserId(1)
+                .map(cart -> {
+                    List<ItemDto> items = new ArrayList<>();
+                    Long total = 0L;
+                    if (cart.getId() != null) {
+                        items = cart.getCartProducts().stream().map(cp -> {
+                            ItemDto item = new ItemDto();
+                            item.setId(cp.getProduct().getId());
+                            item.setTitle(cp.getProduct().getTitle());
+                            item.setDescription(cp.getProduct().getDescription());
+                            item.setImgPath(cp.getProduct().getImgPath());
+                            item.setPrice(cp.getProduct().getPrice());
+                            item.setCount(cp.getQuantity());
+                            return item;
+                        }).toList();
+                        total = cart.getTotalCartPrice();
+                    }
+
+                    return Rendering.view("cart")
+                            .modelAttribute("items", items)
+                            .modelAttribute("total", total)
+                            .build();
+                });
+    }
+
+    @PostMapping(value = "/items")
+    public Mono<String> applyActionToItem(ServerWebExchange swe) {
+
+        Mono<MultiValueMap<String, String>> fd = swe.getFormData();
+        Mono<Product> product = fd.flatMap(map -> productService.getProduct(Integer.parseInt(map.getFirst("id"))));
+
+        return Mono.zip(fd, product)
+                .flatMap(tuple -> {
+                            MultiValueMap<String, String> map = tuple.getT1();
+                            Product p = tuple.getT2();
+
+                            return switch (ActionType.valueOf(map.getFirst("action"))) {
+                                case MINUS -> cartService.decreaseProductCount(1, p);
+                                case PLUS -> cartService.increaseProductCount(1, p);
+                                case DELETE -> cartService.deleteProduct(1, p);
+                            };
+                        }
+                ).thenReturn("redirect:/cart/items");
+    }
 }
