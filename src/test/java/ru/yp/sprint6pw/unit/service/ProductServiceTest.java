73aa70cc2ae5d979ad2c62584e1780a03152eb05
Alexander Korolev <alex.korolev.52rus@gmail.com>
2026-01-12 18:08:22 +0300

    s6: test: Refactor ProductServiceTest

--- src/test/java/ru/yp/sprint6pw/unit/service/ProductServiceTest.java
+++ src/test/java/ru/yp/sprint6pw/unit/service/ProductServiceTest.java
@@ -3,7 +3,10 @@ package ru.yp.sprint6pw.unit.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import ru.yp.sprint6pw.MyMarketApplicationTest;
 import ru.yp.sprint6pw.model.Product;
 import ru.yp.sprint6pw.repository.ProductRepository;
@@ -11,7 +14,6 @@ import ru.yp.sprint6pw.service.ProductService;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -23,66 +25,69 @@ public class ProductServiceTest extends MyMarketApplicationTest {
     @MockitoBean
     private ProductRepository productRepository;
 
+    @MockitoBean
+    private CommandLineRunner commandLineRunner;
+
     @Autowired
     private ProductService productService;
 
-//    @BeforeEach
-//    void resetMocks() {
-//        reset(productRepository);
-//    }
+    @BeforeEach
+    void resetMocks() {
+        reset(productRepository);
+    }
 
-//    @Test
-//    void testGetAllProducts() {
-//        Product p1 = new Product(1, "Prod1", "", "", 1000L);
-//        Product p2 = new Product(2, "Prod2", "", "", 2000L);
-//
-//        List<Product> products = new ArrayList<>();
-//        products.add(p1);
-//        products.add(p2);
-//
-//        doReturn(products).when(productRepository).findAll();
-//        List<Product> result = productService.getAllProducts();
-//
-//        assertNotNull(result, "Product list should not be null");
-//
-//        assertEquals(products.size(), result.size(), "Wrong size of retrieved Orders list");
-//        verify(productRepository, times(1)).findAll();
-//    }
+    @Test
+    void testGetAllProducts() {
+        Product p1 = new Product(1, "Prod1", "", "", 1000L);
+        Product p2 = new Product(2, "Prod2", "", "", 2000L);
 
-//    @Test
-//    void testGetProduct() {
-//        Integer productId = 1;
-//        Product p1 = new Product(productId, "Prod1", "", "", 1000L);
-//
-//        doReturn(Optional.of(p1)).when(productRepository).findById(productId);
-//        Product result = productService.getProduct(productId);
-//
-//        assertNotNull(p1, "Product was not found");
-//
-//        assertEquals(productId, result.getId(), "Product with wrong id was retrieved");
-//        verify(productRepository, times(1)).findById(productId);
-//    }
+        List<Product> products = new ArrayList<>();
+        products.add(p1);
+        products.add(p2);
 
-//    @Test
-//    void testCreate() {
-//        Integer productId = 1;
-//        Product p1 = new Product(productId, "Prod1", "", "", 1000L);
-//
-//        doReturn(p1).when(productRepository).save(p1);
-//        Product result = productService.create(p1);
-//
-//        assertNotNull(p1, "Product was not created");
-//        verify(productRepository, times(1)).save(p1);
-//    }
+        doReturn(Flux.fromIterable(products)).when(productRepository).findAll();
 
-//    @Test
-//    void testUpdate() {
-//        Integer productId = 1;
-//        Product p1 = new Product(productId, "Prod1", "", "", 1000L);
-//
-//        doReturn(p1).when(productRepository).save(p1);
-//        productService.update(p1);
-//
-//        verify(productRepository, times(1)).save(p1);
-//    }
+        List<Product> result = productService.getAllProducts().collectList().block();
+
+        assertNotNull(result, "Product list should not be null");
+        assertEquals(products.size(), result.size(), "Wrong size of retrieved Orders list");
+        verify(productRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testGetProduct() {
+        Integer productId = 1;
+        Product p1 = new Product(productId, "Prod1", "", "", 1000L);
+
+        doReturn(Mono.just(p1)).when(productRepository).findById(productId);
+
+        Product result = productService.getProduct(productId).block();
+
+        assertNotNull(p1, "Product was not found");
+        assertEquals(productId, result.getId(), "Product with wrong id was retrieved");
+        verify(productRepository, times(1)).findById(productId);
+    }
+
+    @Test
+    void testCreate() {
+        Integer productId = 1;
+        Product p1 = new Product(productId, "Prod1", "", "", 1000L);
+
+        doReturn(Mono.just(p1)).when(productRepository).save(p1);
+        Product result = productService.create(p1).block();
+
+        assertNotNull(p1, "Product was not created");
+        verify(productRepository, times(1)).save(p1);
+    }
+
+    @Test
+    void testUpdate() {
+        Integer productId = 1;
+        Product p1 = new Product(productId, "Prod1", "", "", 1000L);
+
+        doReturn(Mono.just(p1)).when(productRepository).save(p1);
+        productService.update(p1).block();
+
+        verify(productRepository, times(1)).save(p1);
+    }
 }
