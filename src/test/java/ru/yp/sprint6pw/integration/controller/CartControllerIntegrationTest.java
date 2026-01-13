e45967db2c3e18728a2741de7495bda52b8694f1
Alexander Korolev <alex.korolev.52rus@gmail.com>
2026-01-13 12:57:08 +0300

    s6: test: Integration test refactoring: CartControllerIntegrationTest

--- src/test/java/ru/yp/sprint6pw/integration/controller/CartControllerIntegrationTest.java
+++ src/test/java/ru/yp/sprint6pw/integration/controller/CartControllerIntegrationTest.java
@@ -1,32 +1,51 @@
 package ru.yp.sprint6pw.integration.controller;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.data.domain.Sort;
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
 
 public class CartControllerIntegrationTest extends MyMarketApplicationWebTest {
 
-//    @Test
-//    void getItems_returnsViewCart() throws Exception {
-//        mockMvc.perform(get("/cart/items"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType("text/html;charset=UTF-8"))
-//                .andExpect(view().name("cart"))
-//                .andExpect(model().attributeExists("items"))
-//                .andExpect(model().attribute("items", hasSize(2)))
-//                .andExpect(model().attributeExists("total"));
-//    }
+    @Test
+    void getItems_returnsViewCart() throws Exception {
 
-//    @Test
-//    void applyPlusActionToItems_returnsRedirectToCartItems() throws Exception {
-//        Integer productId = productRepository.findAll(Sort.by("id").descending()).getFirst().getId();
-//        mockMvc.perform(post("/cart/items?id=" + productId + "&action=PLUS"))
-//                .andExpect(status().is3xxRedirection())
-//                .andExpect(redirectedUrl("/cart/items"));
-//    }
+        webTestClient.get()
+                .uri("/cart/items")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.TEXT_HTML)
+                .expectBody(String.class)
+                .value(html -> {
+                    assert html.contains("<h5 class=\"card-title\">Prod1</h5>");
+                    assert html.contains("<span class=\"badge text-bg-success justify-content-end\">1000 руб.</span>");
+                    assert html.contains("<span>2</span>");
+                    assert html.contains("<h5 class=\"card-title\">Prod2</h5>");
+                    assert html.contains("<span class=\"badge text-bg-success justify-content-end\">2000 руб.</span>");
+                    assert html.contains("<span>4</span>");
+                    assert html.contains("<h2>Итого: 10000 руб.</h2>");
+
+                });
+    }
+
+    @Test
+    void applyPlusActionToItems_returnsRedirectToCartItems() throws Exception {
+        Integer productId = productRepository.findProductsByCriterias("%prod1%", 0, 5)
+                .collectList()
+                .block()
+                .getFirst()
+                .getId();
+
+        MultiValueMap<String,String> formData = new LinkedMultiValueMap<>();
+        formData.add("id", productId.toString());
+        formData.add("action", "PLUS");
+
+        webTestClient.post()
+                .uri("/cart/items")
+                .body(BodyInserters.fromFormData(formData))
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueEquals("Location", "/cart/items");
+    }
 }
