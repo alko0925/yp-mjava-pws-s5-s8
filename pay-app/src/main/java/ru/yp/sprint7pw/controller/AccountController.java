54970b34c843d285a6b9bb197281717ff2fa75c8
Alexander Korolev <alex.korolev.52rus@gmail.com>
2026-01-28 18:59:13 +0300

    s7: feat: Add Controller for pay-app module: AccountController

--- pay-app/src/main/java/ru/yp/sprint7pw/controller/AccountController.java
+++ pay-app/src/main/java/ru/yp/sprint7pw/controller/AccountController.java
@@ -1,4 +1,30 @@
 package ru.yp.sprint7pw.controller;
 
-public class AccountController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import ru.yp.sprint7pw.service.AccountService;
+
+@RestController
+public class AccountController implements AccountApi {
+
+    private final AccountService accountService;
+    private final int DEFAULT_USER_ID = 1;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @Override
+    public Mono<ResponseEntity<Double>> getBalance(Integer userId, ServerWebExchange exchange) {
+        return accountService.getBalanceByUserId(userId)
+                .map(balance -> ResponseEntity.ok().body(balance));
+    }
+
+    @Override
+    public Mono<ResponseEntity<Double>> updateBalance(Integer userId, String operation, Double amount, ServerWebExchange exchange) {
+        return accountService.updateBalanceByUserId(userId, operation, amount)
+                .map(balance -> ResponseEntity.ok().body(balance));
+    }
 }
