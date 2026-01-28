f706c462afea525ac49ea9ac4feb2a62cd86dbd2
Alexander Korolev <alex.korolev.52rus@gmail.com>
2026-01-28 18:50:41 +0300

    s7: data: Add initial data for pay-app module

--- pay-app/src/main/java/ru/yp/sprint7pw/PayApplication.java
+++ pay-app/src/main/java/ru/yp/sprint7pw/PayApplication.java
@@ -4,14 +4,25 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import ru.yp.sprint7pw.model.Account;
+import ru.yp.sprint7pw.model.User;
+import ru.yp.sprint7pw.repository.AccountRepository;
+import ru.yp.sprint7pw.repository.UserRepository;
 
-
-import java.util.List;
 
 @SpringBootApplication
 public class PayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PayApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(UserRepository userRepository, AccountRepository accountRepository) {
+        return args -> {
+            userRepository.save(new User("Ivan", "Ivanov", "ivan.ivanov@payapp.com"))
+                    .flatMap(user -> accountRepository.save(new Account(user.getId(), 10000D)))
+                    .subscribe();
+        };
     }
 }
