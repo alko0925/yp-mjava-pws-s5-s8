0c07f71e401034416e334477f99903952fe9c7a0
Alexander Korolev <alex.korolev.52rus@gmail.com>
2026-01-29 13:31:47 +0300

    s7: test: Integration test implementation: Controllers (pay-app)

---
+++ pay-app/src/test/java/ru/yp/sprint7pw/integration/controller/AccountControllerIntegrationTest.java
@@ -0,0 +1,41 @@
+package ru.yp.sprint7pw.integration.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+public class AccountControllerIntegrationTest extends PayApplicationWebTest {
+
+    @Test
+    void getBalance_returnsCurrentUserBalance() throws Exception {
+        Integer userId = userRepository.findAll()
+                .collectList()
+                .block()
+                .getFirst()
+                .getId();
+
+        webTestClient.get()
+                .uri("/api/accounts/" + userId)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$").isEqualTo(10000);
+    }
+
+    @Test
+    void updateBalance_returnsUpdatedUserBalance() throws Exception {
+        Integer userId = userRepository.findAll()
+                .collectList()
+                .block()
+                .getFirst()
+                .getId();
+
+        webTestClient.put()
+                .uri("/api/accounts/" + userId + "?operation=deposit&amount=1000")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$").isEqualTo(11000);
+    }
+}
--- pay-app/src/test/java/ru/yp/sprint7pw/integration/controller/PayApplicationWebTest.java
+++ pay-app/src/test/java/ru/yp/sprint7pw/integration/controller/PayApplicationWebTest.java
@@ -5,16 +5,37 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import ru.yp.sprint7pw.PayApplicationTest;
+import ru.yp.sprint7pw.model.Account;
+import ru.yp.sprint7pw.model.User;
+import ru.yp.sprint7pw.repository.AccountOperationRepository;
+import ru.yp.sprint7pw.repository.AccountRepository;
+import ru.yp.sprint7pw.repository.UserRepository;
 
 public class PayApplicationWebTest extends PayApplicationTest {
+
     @Autowired
     protected WebTestClient webTestClient;
 
+    @Autowired
+    protected UserRepository userRepository;
+
+    @Autowired
+    protected AccountRepository accountRepository;
+
+    @Autowired
+    protected AccountOperationRepository accountOperationRepository;
+
     @BeforeEach
     void setup() {
+        userRepository.save(new User("Ivan", "Ivanov", "ivan.ivanov@payapp.com"))
+                .flatMap(user -> accountRepository.save(new Account(user.getId(), 10000D)))
+                .block();
     }
 
     @AfterEach
     void cleanUp() {
+        accountRepository.deleteAll()
+                .then(userRepository.deleteAll())
+                .block();
     }
 }
